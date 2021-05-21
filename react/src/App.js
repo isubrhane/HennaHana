01302aa04bf281b2788f8ff018e84b532a7ec406
Isu-brhane <isubrhane77@gmail.com>
2021-05-21 14:55:52 -0400

    updat files

---
+++ react/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import { Navbar, NavbarBrand } from 'reactstrap';
+// import Directory from './components/DirectoryComponent';
+import './App.css';
+import Home from './pages/home';
+
+function App() {
+   
+        return (
+            <Home/>
+        );
+    }
+
+
+export default App;
